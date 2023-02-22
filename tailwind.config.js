43e3724ca235aee0f3014745b84da86dc38348c0
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-02-22 14:18:39 +0700

    feat: setting container + add font noto sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        "2xl": "13rem",
+      },
+      screens: {
+        "2xl": "1440px",
+      },
+    },
     extend: {
       colors: {
         primary: "#6016FC",
@@ -15,6 +28,10 @@ module.exports = {
         red: "#FC165B",
         "primary-light": "#6016FC1A",
         "button-light": "#FFFFFF1A",
+        muted: "#FFFFFF99",
+      },
+      fontFamily: {
+        sans: ["Noto Sans", ...defaultTheme.fontFamily.sans],
       },
     },
   },
