43ef3a1bb8f0f740d61f526bfb5562ee21a75df8
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-03-08 13:58:25 +0700

    refactor: add margin bottom

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ import { Button } from "@src/components/ui/button";
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <LayoutRoot>
-      <section className="container mt-[137px] text-center">
+      <section className="container mt-[137px] mb-[123px] text-center">
         <p className="section-badge text-red">ERROR</p>
         <h1 className="page-title mt-[13px] text-white">
           The page you are looking <br /> for is not on this website, <br />{" "}
