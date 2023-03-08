7d6af635b694e4942398de3eb9decc1b1975cc4e
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-03-08 13:58:58 +0700

    feat: implement services page

---
+++ src/pages/services/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+
+import { LayoutRoot } from "@src/components/layout/layout-root";
+import { Page, PageHeader } from "@src/components/layout/page";
+import { ServicesUiDesign } from "@src/components/services/services-ui-design";
+import { Divider } from "@src/components/ui/divider";
+import { ServicesDevelopment } from "@src/components/services/services-development";
+import { ServicesMaintenance } from "@src/components/services/services-maintenance";
+
+const ServicesPage: React.FC<PageProps> = () => {
+  return (
+    <LayoutRoot>
+      <Page>
+        <PageHeader
+          title="SERVICES"
+          description="We are here to help solve your company's problems"
+        />
+        <Divider className="mt-[89px]" />
+        <ServicesUiDesign />
+        <ServicesDevelopment />
+        <ServicesMaintenance />
+      </Page>
+    </LayoutRoot>
+  );
+};
+
+export default ServicesPage;
+
+export const Head: HeadFC = () => <title>Services Page</title>;
