d5444bad83afe7a5e166b555f51f95c4b74f38a6
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-03-13 20:15:47 +0700

    refactor: add key value list icons

--- src/components/ui/card/list-icons.tsx
+++ src/components/ui/card/list-icons.tsx
@@ -1,17 +1,61 @@
 import * as React from "react";
 
+import { cn } from "@src/lib/utils";
+
 type Props = {
   Icon: React.ElementType;
+  text?: string;
   value: string;
+  keyValue?: boolean;
+  reverse?: boolean;
+  statistic?: boolean;
 };
 
-export function ListIcons({ Icon, value, ...rest }: Props) {
+export function ListIcons({
+  Icon,
+  text,
+  value,
+  keyValue,
+  reverse,
+  statistic,
+  ...rest
+}: Props) {
+  if (keyValue) {
+    return (
+      <li className="flex items-center gap-x-7" {...rest}>
+        <figure className="flex h-[70px] w-[70px] items-center justify-center rounded-[3px] bg-light">
+          <Icon size={30} className="text-white" />
+        </figure>
+        <div className={cn("flex", reverse ? "flex-col-reverse" : "flex-col")}>
+          <p
+            className={cn(
+              "text-muted",
+              statistic ? "statistic-label" : "icon-list-label",
+            )}
+          >
+            {text}
+          </p>
+          <h3
+            className={cn(
+              "text-white",
+              statistic ? "statistic-value" : "icon-list-value",
+            )}
+          >
+            {value}
+          </h3>
+        </div>
+      </li>
+    );
+  }
+
   return (
-    <li className="flex items-center gap-x-[25px] rounded-[3px]" {...rest}>
-      <figure className="flex h-[50px] w-[50px] items-center justify-center bg-light">
+    <li className="flex items-center gap-x-7" {...rest}>
+      <figure className="flex h-[50px] w-[50px] items-center justify-center rounded-[3px] bg-light">
         <Icon size={20} className="text-white" />
       </figure>
-      <p className="section-paragraph text-white">{value}</p>
+      <div>
+        <h3 className="section-paragraph text-white">{value}</h3>
+      </div>
     </li>
   );
 }
