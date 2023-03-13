5d2e7c6358d1dd2794af4319430009f0e6ab10bd
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-03-13 20:09:34 +0700

    refactor: add link

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 import { Button } from "@src/components/ui/button";
 
@@ -19,9 +20,11 @@ export function ProjectCard({
       <figure>{img}</figure>
       <h3 className="project-title mt-10 text-white">{title}</h3>
       <p className="project-description mt-[10px] text-muted">{description}</p>
-      <Button variant="outline-white" rounded="pill" className="mt-[38px]">
-        Detail
-      </Button>
+      <Link to="/project/detail">
+        <Button variant="outline-white" rounded="pill" className="mt-[38px]">
+          Detail
+        </Button>
+      </Link>
     </li>
   );
 }
