68515087598e5c2a1351c8dfd4b928bed1cc0d17
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-03-13 20:13:37 +0700

    refactor: add container component

--- src/components/layout/page/page-header.tsx
+++ src/components/layout/page/page-header.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { Badge } from "@src/components/ui/section/badge";
+import { Container } from "@src/components/ui/container";
 
 type PageHeaderProps = {
   badge: string;
@@ -10,14 +11,14 @@ type PageHeaderProps = {
 export function PageHeader({ badge, title }: PageHeaderProps) {
   return (
     <header className="mt-[101px]">
-      <div className="container">
+      <Container className="container">
         <div className="text-center">
           <Badge>{badge}</Badge>
           <h1 className="page-title mx-auto mt-1 max-w-[507px] text-white">
             {title}
           </h1>
         </div>
-      </div>
+      </Container>
     </header>
   );
 }
